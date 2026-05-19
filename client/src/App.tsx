6476196080fa23a616a41600b4a6afd643a7615b
Sintu <sinjaa18@gmail.com>
2026-05-19 23:07:58 +0530

    Fix dashboard import casing

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,7 +2,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom"
 
 import RegisterPage from "./pages/RegisterPage"
 import LoginPage from "./pages/LoginPage"
-import DashboardPage from "./pages/Dashboardpage"
+import DashboardPage from "./pages/DashboardPage"
 
 const App = () => {
   return (
